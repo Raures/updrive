5055cd3d1f4b490e5f0d2e9d13f838493bd7b7fb
Raures <raures778@gmail.com>
2024-10-05 18:11:16 +0300

    * refactor: Add logger * perf: Move logic inside the if statement

--- src/main/java/com/rmunteanu/updrive/service/ScheduledTask.java
+++ src/main/java/com/rmunteanu/updrive/service/ScheduledTask.java
@@ -4,6 +4,8 @@ import com.rmunteanu.updrive.entity.FileMetadata;
 import com.rmunteanu.updrive.repository.FileRepository;
 import lombok.RequiredArgsConstructor;
 import org.apache.tomcat.util.http.fileupload.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -17,23 +19,29 @@ import java.util.Objects;
 @RequiredArgsConstructor
 public class ScheduledTask {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ScheduledTask.class);
+
     private final FileRepository fileRepository;
     private final Environment environment;
 
-    @Scheduled(fixedRate = 5000)
+    @Scheduled(fixedRate = 30000)
     public void deleteExpiredFiles() {
         List<FileMetadata> expiredFileMetadata = fileRepository.readByExpirationDate();
         if (!expiredFileMetadata.isEmpty()) {
+            if (LOGGER.isDebugEnabled()) {
+                LOGGER.debug("Identified {} expired files to be deleted.", expiredFileMetadata.size());
+                LOGGER.debug("Saving expired status to the database.");
+            }
             expiredFileMetadata.forEach(fm -> fm.setExpired(true));
             fileRepository.saveAll(expiredFileMetadata);
-        }
-        String dataFolder = Objects.requireNonNull(environment.getProperty("file.data.directory"));
-        try {
-            for (FileMetadata fileMetadata : expiredFileMetadata) {
-                FileUtils.deleteDirectory(Path.of(dataFolder, fileMetadata.getSlotId()).toFile());
+            String dataFolder = Objects.requireNonNull(environment.getProperty("file.data.directory"));
+            try {
+                for (FileMetadata fileMetadata : expiredFileMetadata) {
+                    FileUtils.deleteDirectory(Path.of(dataFolder, fileMetadata.getSlotId()).toFile());
+                }
+            } catch (IOException e) {
+                LOGGER.error("An error occurred while deleting expired files.", e);
             }
-        } catch (IOException e) {
-            System.out.println("Failed to delete expired files." + e.getMessage());
         }
     }
 }
