411bc461a83a15378fbb9e4ec6c899cee9c42bf2
Raures <raures778@gmail.com>
2024-10-07 22:01:41 +0300

    * feat: Integrate dedicated REST exceptions for download service

--- src/main/java/com/rmunteanu/updrive/service/FileDownloadService.java
+++ src/main/java/com/rmunteanu/updrive/service/FileDownloadService.java
@@ -1,5 +1,6 @@
 package com.rmunteanu.updrive.service;
 
+import com.rmunteanu.updrive.controller.exception.*;
 import com.rmunteanu.updrive.entity.FileMetadata;
 import com.rmunteanu.updrive.repository.FileRepository;
 import org.slf4j.Logger;
@@ -30,30 +31,36 @@ public class FileDownloadService {
         FileMetadata fileMetadata = fileRepository.readBySlotId(slotId);
         Path filePath;
         if (fileMetadata == null) {
-            LOGGER.error("Slot ID does not exist.");
-            throw new RuntimeException("Slot ID does not exist.");
+            LOGGER.error("Slot ID {} does not exist.", slotId);
+            throw new SlotNotFoundRuntimeException("Slot ID %s does not exist.", slotId);
         }
         if (fileMetadata.isExpired()) {
             LOGGER.error("Slot has expired on {}.", fileMetadata.getExpirationDate());
-            throw new RuntimeException("Slot has expired on " + fileMetadata.getExpirationDate() + ".");
+            throw new SlotExpiredRuntimeException("Slot has expired on %s.", fileMetadata.getExpirationDate());
         }
         String dataFolder = Objects.requireNonNull(environment.getProperty("file.data.directory"));
+        if (LOGGER.isDebugEnabled()) {
+            LOGGER.debug("Reading contents of folder: {}", dataFolder);
+        }
         Path filesPath = Path.of(dataFolder, slotId);
         if (Files.exists(filesPath)) {
             File filesFolder = filesPath.toFile();
             File[] files = filesFolder.listFiles();
-            if (files.length == 0) {
+            if (files == null || files.length == 0) {
                 LOGGER.error("Slot ID exists, but no files were found.");
-                throw new RuntimeException("Slot ID exists, but no files were found.");
+                throw new NoFileRuntimeException("Slot ID exists, but no files were found.");
             } else if (files.length == 1) {
+                if (LOGGER.isDebugEnabled()) {
+                    LOGGER.debug("Returning file for slot ID {}.", slotId);
+                }
                 filePath = files[0].toPath();
             } else {
-                LOGGER.error("Slot ID exists but something must've gone wrong during the upload process. Too many files!");
-                throw new RuntimeException("Slot ID exists but something must've gone wrong during the upload process. Too many files!");
+                LOGGER.error("Should read exactly one file for a successful file upload. Slot ID {} returned {} files.", slotId, files.length);
+                throw new TooManyFilesRuntimeException("Slot ID exists but something must've gone wrong during the upload process. Too many files!");
             }
         } else {
             LOGGER.error("Path does not exist: {}", filesPath);
-            throw new RuntimeException("Path does not exist: " + filesPath);
+            throw new NoFileRuntimeException("Path does not exist: %s", filesPath);
         }
         return filePath;
     }
