0e7c3673ce50f8175924c7191b85b5f77ea5935c
Raures <raures778@gmail.com>
2024-10-13 19:54:19 +0300

    * fix: Replace @Controller with @RestController

--- src/main/java/com/rmunteanu/updrive/controller/FileDownloadController.java
+++ src/main/java/com/rmunteanu/updrive/controller/FileDownloadController.java
@@ -6,17 +6,17 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 
-@Controller
+@RestController
 @RequestMapping("api/v1/download")
 public class FileDownloadController {
 
